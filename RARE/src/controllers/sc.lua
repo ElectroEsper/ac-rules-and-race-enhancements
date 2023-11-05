6bfe517a69360913fe639ad1f6baa22c1729704b
William <ElectroEsper@users.noreply.github.com>
2023-11-05 11:21:14 -0500

    Create sc.lua
    Signed-off-by: William <ElectroEsper@users.noreply.github.com>

---
+++ RARE/src/controllers/sc.lua
@@ -0,0 +1,6 @@
+local SAFETYCAR_DEPLOYED = false
+local SAFETYCAR_INPIT = true
+local SAFETYCAR_CALLEDIN = false
+local SAFETYCAR_MAXLAPS = 3
+local SAFETYCAR_COMPLETEDLAPS = 0
+local SAFETYCAR_CAR = nil -- insert car type used for safety car
